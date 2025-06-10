f651532fcaeb7d8e9f15fa8ab4ebfd2a12255b57
rlim27 <rlim27@kellettschool.com>
2025-06-10 02:29:02 +0000

    Jun 10, 2025, 2:29 AM

--- main.py
+++ main.py
@@ -6,53 +6,55 @@ from termcolor import colored # pip install termcolor
 
 
 
-chosenwords = ["FUZZY", "LUTES", "ANKLE", "PARCH", "ROCKY", "THYME", "DRYAD", "OUGHT", "BORED", "LEANS", "STOMP", "ERASE", "ENDER", "SPEAR", "PRIED", "IRATE", "SCRAP", "CHARM", "SITCH", "SPARK", "QUEEN", "ICHOR", "CRAMP", "LAMPS", "AIRED", "FRIED", "ARISE", "CHORD", "SHANK", "FEAST", "ASHEN"] 
-rightguess = False
-corrections = [0,0,0,0,0]
-imitations = ["O","O","O","O","O"]
-correctword = chosenwords[random.randint(0,28)]
 
 
 print("Welcome to wordle! Guess a 5 letter word within 6 tries. Each letter in your word will be marked as correct, in the wrong place, or incorrect. When all the letters in your word match the letters in the correct word, a good job message will be returned. Play.") 
 
-count = 0
-while count <= 5 and rightguess == False:
+def playround():
+    chosenwords = ["FUZZY", "LUTES", "ANKLE", "PARCH", "ROCKY", "THYME", "DRYAD", "OUGHT", "BORED", "LEANS", "STOMP", "ERASE", "ENDER", "SPEAR", "PRIED", "IRATE", "SCRAP", "CHARM", "SITCH", "SPARK", "QUEEN", "ICHOR", "CRAMP", "LAMPS", "AIRED", "FRIED", "ARISE", "CHORD", "SHANK", "FEAST", "ASHEN"] 
+    rightguess = False
+    corrections = [0,0,0,0,0]
+    correctword = chosenwords[random.randint(0,28)]
+    wins = 0
+    losses = 0
+    count = 0
+    while count <= 5 and rightguess == False:
+        guess = input("Guess:")
+        guess = guess.upper()
+        if len(guess) != 5: 
+            print("Wrong length")
 
-    guess = input("Guess:")
-    guess = guess.upper()
-    if len(guess) != 5: 
-        print("Wrong length")
-
-    else: 
-        for letter in range(5): 
-            if guess[letter] == correctword[letter]:
-                corrections[letter] = 2
-            elif guess[letter] in correctword:
-                corrections[letter] = 1
-            else: 
-                corrections[letter] = 0
+        else: 
+            for letter in range(5): 
+                if guess[letter] == correctword[letter]:
+                    corrections[letter] = 2
+                elif guess[letter] in correctword:
+                    corrections[letter] = 1
+                else: 
+                    corrections[letter] = 0
 
 
-        print(corrections)#change to imitations
+            print(corrections)#change to imitations
 
-        if (not(0 in corrections)) and (not(1 in corrections)):
-            rightguess = True 
+            if (not(0 in corrections)) and (not(1 in corrections)):
+                rightguess = True 
         
-        count += 1
-
-
-
-for i in range(5):
-    if imitations[i] == 0:
-        imitations[i] = imitations[i] # red
-    elif imitations[i] == 1:
-        imitations[i] = imitations[i] #yellow
+            count += 1
+    if rightguess == True:
+        print('Word Guessed')
+        print(correctword)
+        wins += 1
     else:
-        imitations[i] = imitations[i] #Green
+        print('You Lost')
+        print(correctword)
+        losses += 1
+    return wins, losses, rightguess
 
-if rightguess == True:
-    print('Word Guessed')
-    print(correctword)
-else:
-    print('you lose')
-    print(correctword)
+def displaystats():
+    stats = ("wins:", wins, "losses:", losses)
+
+choice = input("Select Menu Option")
+if choice == "P" or choice == 'p':
+    playround()
+elif choice == 'S' or choice == 's':
+    displaystats()
