dfeaad0ecb66ccdd677b0941a35ad9b0e0cf9c4a
rlim27 <rlim27@kellettschool.com>
2025-06-10 02:57:49 +0000

    Jun 10, 2025, 2:57 AM

--- main.py
+++ main.py
@@ -58,8 +58,8 @@ def displaystats(wins, losses):
 
     return stats
 
-choice = input("Select Menu Option")
 
+choice = input("Select Menu Option: Play Round = P, Display Stats = S")
 while choice in "PpSs":
     if choice == 'P' or choice == 'p':
         playround()
